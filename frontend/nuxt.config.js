3ec7a44ece1329c2f1b9ae461f5a91d05760b1fa
Effie <effasj@yandex.ru>
2025-03-21 21:09:23 +0300

    init commit

---
+++ frontend/nuxt.config.js
@@ -0,0 +1,32 @@
+
+export default {
+  mode: 'universal',
+
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  loading: { color: '#fff' },
+  css: [],
+  plugins: [],
+  buildModules: [],
+  modules: [
+    '@nuxtjs/axios',
+    'nuxt-buefy'
+  ],
+   axios: {
+    baseURL: process.env.API_BASE_URL
+  },
+  build: {
+    extend (config, ctx) {
+    }
+  }
+}
